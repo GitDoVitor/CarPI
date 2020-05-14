e9d8b5a9285289cfea45ba59b8e113f72a3bcb5d
Joao Vitor <joaovitorsantoscs@gmail.com>
2020-05-14 20:18:41 -0300

    arrumando código

--- APIRest/src/main/java/com/timeot4/carpi/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.timeot4.carpi.repository;
-
-import com.timeot4.carpi.models.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    Produto findById(long id);
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# CarPI
-
-#Link para o Exemplo que o professor mandou: 
-https://medium.com/@msealvial/blindando-sua-api-spring-boot-com-o-padr%C3%A3o-dto-44f97020d1a0
